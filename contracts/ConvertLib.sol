7c11b9355fe08527c081346e6f1e9068ac97fec7
Mike Pumphrey <mike.pumphrey@consensys.net>
2017-12-28 17:05:44 -0800

    Eliminating compilation warnings and adding default compatibility with Ganache

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.17;
 
 library ConvertLib{
-	function convert(uint amount,uint conversionRate) returns (uint convertedAmount)
-	{
-		return amount * conversionRate;
-	}
+    function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
+    {
+        return amount * conversionRate;
+    }
 }
--- truffle.js
+++ truffle.js
@@ -4,8 +4,8 @@ require('babel-register')
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
-      port: 8545,
+      host: '127.0.0.1',
+      port: 7545,
       network_id: '*' // Match any network id
     }
   }
