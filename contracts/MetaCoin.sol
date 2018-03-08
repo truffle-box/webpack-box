327b0a2842bcd225cbeac561b220ae43a565d77d
Charles Lee <chug2k@gmail.com>
2018-03-08 22:21:13 +0700

    Fixing sollint errors.

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -17,15 +17,16 @@ contract MetaCoin {
 	}
 
 	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
+		if (balances[msg.sender] < amount) 
+			return false;
 		balances[msg.sender] -= amount;
 		balances[receiver] += amount;
 		Transfer(msg.sender, receiver, amount);
 		return true;
 	}
 
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
+	function getBalanceInEth(address addr) public view returns(uint) {
+		return ConvertLib.convert(getBalance(addr), 2);
 	}
 
 	function getBalance(address addr) public view returns(uint) {
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -5,7 +5,8 @@ contract Migrations {
   uint public last_completed_migration;
 
   modifier restricted() {
-    if (msg.sender == owner) _;
+    if (msg.sender == owner) 
+      _;
   }
 
   function Migrations() public {
@@ -16,8 +17,8 @@ contract Migrations {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
+  function upgrade(address newAddress) public restricted {
+    Migrations upgraded = Migrations(newAddress);
     upgraded.setCompleted(last_completed_migration);
   }
 }
