641c36118f4a272bb1c5e10abac4f7acdda24472
Dror Tirosh <dror@tabookey.com>
2018-12-10 14:37:17 +0200

    lint error, package.json

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,23 +3,21 @@ var MetaCoin = artifacts.require('./MetaCoin.sol')
 var RelayHub = artifacts.require('RelayHub')
 
 module.exports = function (deployer) {
-
-	var meta
+  var meta
   deployer.deploy(ConvertLib)
   deployer.link(ConvertLib, MetaCoin)
   deployer.deploy(MetaCoin).then(dep => {
-      meta = dep
-	  return RelayHub.at("0x254dffcd3277c0b1660f6d42efbb754edababc2b")
-	}).then(rhub=> {
-	  console.log( "added deposit for META "+meta.address)
-	  return rhub.depositFor(meta.address, { value: 1e17 })
-	}).then(ret=>{
-
-	  	console.log( "depositFor completed ")
-	  	return meta.getDeposit.call()
-  	}).then((value)=>{
-	  	console.log( "getDeposit(",meta.address,") = ",value.toNumber())
-  	}).catch( e=> {
-	  	console.log( "error:",e)
-  	})
+    meta = dep
+    return RelayHub.at('0x254dffcd3277c0b1660f6d42efbb754edababc2b')
+  }).then(rhub => {
+    console.log('added deposit for META ' + meta.address)
+    return rhub.depositFor(meta.address, { value: 1e17 })
+  }).then(ret => {
+    console.log('depositFor completed ')
+    return meta.getDeposit.call()
+  }).then((value) => {
+    console.log('getDeposit(', meta.address, ') = ', value.toNumber())
+  }).catch(e => {
+    console.log('error:', e)
+  })
 }
