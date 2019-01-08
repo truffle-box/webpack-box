32554e1aac2d77a4a50b20ae472595e8d29026b3
Adrian Li <li.adrianmc@gmail.com>
2019-01-08 11:50:10 -0500

    add tests and make them pass

---
+++ test/metacoin.js
@@ -0,0 +1,76 @@
+var MetaCoin = artifacts.require('./MetaCoin.sol')
+
+contract('MetaCoin', function (accounts) {
+  it('should put 10000 MetaCoin in the first account', function () {
+    return MetaCoin.deployed().then(function (instance) {
+      return instance.getBalance.call(accounts[0])
+    }).then(function (balance) {
+      assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account")
+    })
+  })
+  it('should call a function that depends on a linked library', function () {
+    var meta
+    var metaCoinBalance
+    var metaCoinEthBalance
+
+    return MetaCoin.deployed().then(function (instance) {
+      meta = instance
+      return meta.getBalance.call(accounts[0])
+    }).then(function (outCoinBalance) {
+      metaCoinBalance = outCoinBalance.toNumber()
+      return meta.getBalanceInEth.call(accounts[0])
+    }).then(function (outCoinBalanceEth) {
+      metaCoinEthBalance = outCoinBalanceEth.toNumber()
+    }).then(function () {
+      assert.equal(
+        metaCoinEthBalance,
+        2 * metaCoinBalance,
+        'Library function returned unexpeced function, linkage may be broken'
+      )
+    })
+  })
+
+  it('should send coin correctly', function () {
+    var meta
+
+    //    Get initial balances of first and second account.
+    var accountOne = accounts[0]
+    var accountTwo = accounts[1]
+
+    var accountOneStartingBalance
+    var accountTwoStartingBalance
+    var accountOneEndingBalance
+    var accountTwoEndingBalance
+
+    var amount = 10
+
+    return MetaCoin.deployed().then(function (instance) {
+      meta = instance
+      return meta.getBalance.call(accountOne)
+    }).then(function (balance) {
+      accountOneStartingBalance = balance.toNumber()
+      return meta.getBalance.call(accountTwo)
+    }).then(function (balance) {
+      accountTwoStartingBalance = balance.toNumber()
+      return meta.sendCoin(accountTwo, amount, { from: accountOne })
+    }).then(function () {
+      return meta.getBalance.call(accountOne)
+    }).then(function (balance) {
+      accountOneEndingBalance = balance.toNumber()
+      return meta.getBalance.call(accountTwo)
+    }).then(function (balance) {
+      accountTwoEndingBalance = balance.toNumber()
+
+      assert.equal(
+        accountOneEndingBalance,
+        accountOneStartingBalance - amount,
+        "Amount wasn't correctly taken from the sender"
+      )
+      assert.equal(
+        accountTwoEndingBalance,
+        accountTwoStartingBalance + amount,
+        "Amount wasn't correctly sent to the receiver"
+      )
+    })
+  })
+})
