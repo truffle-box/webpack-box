36374e4bef55058dea3f21f45a39b6b0bcf43b84
Adrian Li <li.adrianmc@gmail.com>
2019-01-08 11:27:54 -0500

    add and update contracts

---
+++ contracts/ConvertLib.sol
@@ -0,0 +1,7 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+library ConvertLib {
+    function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount) {
+        return amount * conversionRate;
+    }
+}
