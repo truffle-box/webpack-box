97fd0dcc5f16191285c701f54180ad632cf833fa
Andrew Stanger <andrew@stanger.co.uk>
2019-04-01 10:23:22 +0100

    Added development mode to rapdily increase recompile speed

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: "./src/index.js",
   output: {
     filename: "index.js",
